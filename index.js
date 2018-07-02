552a357dff83f308a442a9e0769102df66196d97
ionflare <ccionflare@gmail.com>
2018-07-02 04:35:11 +0000

    text ajax

--- index.js
+++ index.js
@@ -134,7 +134,10 @@ app.get('/sitemap',function(req,res){
 
 
 app.post('/instert_Q_info',function(req,res){
-  res.sendFile(__dirname+'/sitemap.html');
+    
+    res.send("555");
+  //res.send({ some: JSON.stringify({response:'json'}) });
+  //res.sendFile(__dirname+'/sitemap.html');
 });
 
 
